2585508c6ced0d2a1010db23330c94a6e3278965
Hla Myo <hlamyo.louis@gmail.com>
2025-11-04 12:50:00 +0100

    done

--- sum_of_cubes_even.py
+++ sum_of_cubes_even.py
@@ -1,13 +1,11 @@
 def sum_of_cubes_even(n):
-    if n<0:
+    if n<0 or not isinstance(n, int):
         return -1
     if n>2000:
         print("warning")
     total=0
     i=0
     while i<=n:
-        if i%2 == 0:
-            total = total + i**3
-            i= i+1
+        total = total + i**3
+        i= i+2
     return float(total)
-        
